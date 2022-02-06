189484fbfe3935662beed5b246e7002b3dec8393
NesaQ Nes <kholiknes@gmail.com>
2022-02-06 20:15:29 +0300

    step2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,11 +5,9 @@ const program = new Command();
 
 program
   .description('Compares two configuration files and shows a difference.')
-  .version('0.0.1');
-
-program.command('split')
+  .version('0.0.1')
   .arguments('<filepath1> <filepath2>')
-  .option('-h, --help, display help for command')
-  .option('-V, --version, output the version number');
-
+//   .option('-h, --help, display help for command')
+//   .option('-V, --version, output the version number')
+  .option('-f, --format <type>', 'output format');
 program.parse();
