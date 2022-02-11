c6e8114aaa88849b47ea8d3d597853a6fc2f8d55
NesaQ Nes <kholiknes@gmail.com>
2022-02-11 15:32:57 +0300

    test coverage

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,8 +1,6 @@
 import * as fs from 'fs';
-// eslint-disable-next-line import/no-duplicates
 import path from 'path';
 import { fileURLToPath } from 'url';
-// eslint-disable-next-line import/no-duplicates
 import { dirname } from 'path';
 import gendiff from '../src/gendiff.js';
 
@@ -12,13 +10,11 @@ const __dirname = dirname(__filename);
 const getFixturePath = (filenames) => path.join(__dirname, '..', '__fixtures__', filenames);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-// eslint-disable-next-line no-undef
 test('check diff fn', () => {
   const filepath1 = getFixturePath('file1.json');
   const filepath2 = getFixturePath('file2.json');
 
   const actual = gendiff(filepath1, filepath2);
   const expectedResult = readFile('expectedResult.txt');
-  // eslint-disable-next-line no-undef
   expect(actual).toEqual(expectedResult);
 });
