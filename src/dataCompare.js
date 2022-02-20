52cfa3482b09f61012130d1d6f21b10598771f1b
NesaQ Nes <kholiknes@gmail.com>
2022-02-20 16:49:56 +0300

    refactroint plain and dataCompare

--- src/dataCompare.js
+++ src/dataCompare.js
@@ -6,23 +6,22 @@ const treeDiff = (obj1, obj2) => {
   const keys = _.union(keys1, keys2);
   const sortedKeys = _.sortBy(keys);
 
-  const getDiff = (key) => {
+  const nodes = sortedKeys.map((key) => {
     if (_.isPlainObject(obj1[key]) && _.isPlainObject(obj2[key])) {
       return {
         key, children: treeDiff(obj1[key], obj2[key]), type: 'nested',
       };
     }
     if (!_.has(obj1, key)) return { key, children: obj2[key], type: 'added' };
-
     if (!_.has(obj2, key)) return { key, children: obj1[key], type: 'deleted' };
-
     if (obj1[key] !== obj2[key]) {
       return {
         key, children: obj1[key], children2: obj2[key], type: 'changed',
       };
     }
     return { key, children: obj1[key], type: 'unchanged' };
-  };
-  return sortedKeys.map(getDiff);
+  });
+  return nodes;
 };
+
 export default treeDiff;
--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -8,31 +8,24 @@ const stringify = (value) => {
   return _.isString(value) ? `'${value}'` : value;
 };
 
-const makeNode = (nodes) => {
-  const iter = (node, nameKey) => {
-    const currentKey = `${nameKey}${node.key}`;
-    switch (node.type) {
-      case 'nested':
-        return node.children.map((child) => iter(child, `${currentKey}.`)).join('');
-      case 'added':
-        return `Property '${currentKey}' was added with value: ${stringify(node.children)}\n`;
-      case 'deleted':
-        return `Property '${currentKey}' was removed\n`;
-      case 'changed':
-        return `Property '${currentKey}' was updated. From ${stringify(node.children)} to ${stringify(node.children2)}\n`;
-      case 'unchanged':
-        return '';
-      default:
-        throw new Error(`wrong ${node.type}`);
-    }
-  };
-
-  return iter(nodes, '');
+const plain = (tree) => {
+  const format = (nodes, parent) => nodes
+    .filter((node) => node.type !== 'unchanged')
+    .map((node) => {
+      const property = parent ? `${parent}.${node.key}` : node.key;
+      switch (node.type) {
+        case 'nested':
+          return `${format(node.children, property)}`;
+        case 'added':
+          return `Property '${property}' was added with value: ${stringify(node.children)}`;
+        case 'deleted':
+          return `Property '${property}' was removed`;
+        case 'changed':
+          return `Property '${property}' was updated. From ${stringify(node.children)} to ${stringify(node.children2)}`;
+        default:
+          throw new Error(`wrong ${node.type}`);
+      }
+    }).join('\n');
+  return format(tree, 0);
 };
-
-const plain = (nodes) => {
-  const lines = nodes.map((node) => makeNode(node));
-  return lines.join('').trim();
-};
-
 export default plain;
